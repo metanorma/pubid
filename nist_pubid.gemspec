7178b4084fbfa583ff05dd5f03b096b0bec5c402
Artur Komarov <artur.komarov@gmail.com>
2021-12-01 13:30:25 +0700

    Basic implementation

---
+++ nist_pubid.gemspec
@@ -0,0 +1,23 @@
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+Gem::Specification.new do |spec|
+  spec.name          = 'nist-pubid'
+  spec.version       = '0.0.1'
+  spec.summary       = ''
+  spec.authors       = ["Ribose Inc."]
+
+  # Specify which files should be added to the gem when it is released.
+  # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
+  spec.files         = Dir.chdir(File.expand_path(__dir__)) do
+    `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
+  end
+  spec.bindir        = 'exe'
+  spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
+  spec.require_paths = ['lib']
+  spec.required_ruby_version = Gem::Requirement.new('>= 2.5.0')
+
+  spec.add_development_dependency 'pry-byebug'
+  spec.add_development_dependency 'rake', '~> 13.0'
+  spec.add_development_dependency 'rspec', '~> 3.0'
+end
---
+++ spec/nist_pubid_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+# PubID examples (TODO: add to tests)
+# NIST SP 800-40r3
+# NIST SP 800-45ver2
+# NIST SP 800-53r4/Upd 3:2015
+# NIST SP IPD 800-53r5
+# NIST SP 800-53Ar4/Upd 1:2014
+# NIST SP 800-60v1r1
+# NIST SP 800-57pt1r4
+# NIST SP 800-73-4/Upd 1:2016
+# NIST SP 2PD 800-188
+# NIST SP 2PD 1800-13B
+# NIST SP PreD 1800-19B
+# NIST IR 8011v3
+# NIST IR 8204/Upd 1:2019
+# NIST IR 8115(spa)
+# NIST HB 130e2019
+# NIST SP 1041r1/Upd 1:2012
+# NIST NCSTAR 1-1Cv1
+# NIST SP 800-38A has an Addendum
+
+RSpec.describe NistPubid do
+  let(:short_pubid) { 'NIST SP 800-53r5' }
+  let(:long_pubid) { 'National Institute of Standards and Technology Special Publication 800-53, Revision 5' }
+  let(:abbrev_pubid) { 'Natl. Inst. Stand. Technol. Spec. Publ. 800-53, Revision 5' }
+  let(:mr_pubid) { 'NIST.SP.800-53r5' }
+
+  it 'parses NIST PubID using parameters' do
+    expect(NistPubid.new(publisher: :nist, series: 'SP', docnumber: '800-53', revision: 5).to_s(:mr))
+      .to eq(mr_pubid)
+  end
+
+  it 'parses MR NIST PubID' do
+    expect(NistPubid.parse(mr_pubid).to_s(:long)).to eq(long_pubid)
+  end
+
+  describe 'generate NIST PubID string outputs' do
+    subject { NistPubid.parse(short_pubid) }
+
+    it 'converts into long Full PubID' do
+      expect(subject.to_s(:long)).to eq(long_pubid)
+    end
+
+    it 'converts into Abbreviated PubID' do
+      expect(subject.to_s(:abbrev)).to eq(abbrev_pubid)
+    end
+
+    it 'converts into Short PubID' do
+      expect(subject.to_s(:short)).to eq(short_pubid)
+    end
+
+    it 'converts into Machine-readable PubID' do
+      expect(subject.to_s(:mr)).to eq(mr_pubid)
+    end
+  end
+
+  describe 'access to PubID object' do
+    it 'returns revision' do
+      expect(NistPubid.parse(short_pubid).revision).to eq('5')
+    end
+
+    it 'can update revistion' do
+      pub_id = NistPubid.parse(short_pubid)
+      pub_id.revision = 6
+      expect(pub_id.to_s(:mr)).to eq('NIST.SP.800-53r6')
+    end
+  end
+end
