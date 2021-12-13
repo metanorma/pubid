39c8fbec4ba90d4b32bd3bf5be3172c5ee1ce398
Artur Komarov <artur.komarov@gmail.com>
2021-12-13 22:32:56 +0700

    Report documents different/comply with NIST PubID, documents id failed to parse

--- lib/nist_pubid/nist_tech_pubs.rb
+++ lib/nist_pubid/nist_tech_pubs.rb
@@ -6,18 +6,60 @@ module NistPubid
   class NistTechPubs
     URL = "https://raw.githubusercontent.com/usnistgov/NIST-Tech-Pubs/nist-pages/xml/allrecords.xml".freeze
 
-    def self.fetch
-      Nokogiri::XML(URI.open(URL))
-        .xpath("/body/query/doi_record/report-paper/report-paper_metadata")
-        .map { |doc| parse_docid doc }
-    rescue StandardError => e
-      warn e.message
-      []
-    end
+    class << self
+      def fetch
+        Nokogiri::XML(URI.open(URL))
+          .xpath("/body/query/doi_record/report-paper/report-paper_metadata")
+          .map { |doc| parse_docid doc }
+      rescue StandardError => e
+        warn e.message
+        []
+      end
 
-    def self.parse_docid(doc)
-      doc.at("publisher_item/item_number", "publisher_item/identifier").text
-        .sub(%r{^/}, "")
+      def convert(doc)
+        NistPubid::Document.parse(doc[:id]).to_s(:short)
+      rescue Errors::ParseError
+        NistPubid::Document.parse(doc[:doi]).to_s(:short)
+      end
+
+      def parse_docid(doc)
+        id = doc.at("publisher_item/item_number", "publisher_item/identifier")
+          .text.sub(%r{^/}, "")
+        doi = doc.at("doi_data/doi").text.gsub("10.6028/", "")
+        case doi
+        when "10.6028/NBS.CIRC.12e2revjune" then id.sub!("13e", "12e")
+        when "10.6028/NBS.CIRC.36e2" then id.sub!("46e", "36e")
+        when "10.6028/NBS.HB.67suppJune1967" then id.sub!("1965", "1967")
+        when "10.6028/NBS.HB.105-1r1990" then id.sub!("105-1-1990", "105-1r1990")
+        when "10.6028/NIST.HB.150-10-1995" then id.sub!(/150-10$/, "150-10-1995")
+        end
+
+        { id: id, doi: doi }
+      end
+
+      def comply_with_pubid
+        fetch.select do |doc|
+          convert(doc) == doc[:id]
+        rescue Errors::ParseError
+          false
+        end
+      end
+
+      def different_with_pubid
+        fetch.reject do |doc|
+          convert(doc) == doc[:id]
+        rescue Errors::ParseError
+          true
+        end
+      end
+
+      def parse_fail_with_pubid
+        fetch.select do |doc|
+          convert(doc) && false
+        rescue Errors::ParseError
+          true
+        end
+      end
     end
   end
 end
--- spec/nist_pubid/nist_tech_pubs_spec.rb
+++ spec/nist_pubid/nist_tech_pubs_spec.rb
@@ -1,12 +1,62 @@
-RSpec.describe NistPubid::NistTechPubs do
+RSpec.describe NistPubid::NistTechPubs, vcr: true do
   describe "#fetch" do
-    it "fetch and parse doc identifiers from nist_tech_pubs", vcr: true do
-      expect(described_class.fetch)
+    it "fetch doc identifiers from nist_tech_pubs" do
+      expect(described_class.fetch.map { |d| d[:id] })
         .to include("NBS BH 1",
                     "NIST SP 1800-15",
                     "NIST SP 1265",
                     "NBS FIPS 83",
                     "NISTIR 8379")
+    end
+
+    it "fetches doi identifiers" do
+      expect(described_class.fetch.map { |d| d[:doi] })
+        .to include("NBS.BH.1",
+                    "NIST.SP.1800-15",
+                    "NIST.SP.1265",
+                    "NBS.FIPS.83",
+                    "NIST.IR.8379")
+    end
+  end
+
+  describe "#convert" do
+    it "converts old pubid to new NIST PubID" do
+      expect(described_class.convert({ id: "NISTIR 8379" }))
+        .to eq("NIST IR 8379")
+    end
+
+    it "keeps correct NIST PubID the same" do
+      expect(described_class.convert({ id: "NIST SP 800-133r2" }))
+        .to eq("NIST SP 800-133r2")
+      expect(described_class.convert({ id: "NIST SP 800-160v1" }))
+        .to eq("NIST SP 800-160v1")
+    end
+
+    it "uses doi when cannot parse document id" do
+      expect(described_class.convert(
+               { id: "NBS CIRC re3", doi: "NBS.CIRC.5e3" },
+             )).to eq("NBS CIRC 5e3")
+    end
+  end
+
+  describe "#comply_with_pubid" do
+    it "returns identifier comply with NIST PubID" do
+      expect(described_class.comply_with_pubid.map { |d| d[:id] })
+        .to include("NIST SP 800-133r2")
+    end
+  end
+
+  describe "#different_with_pubid" do
+    it "returns identifiers not comply with NIST PubID" do
+      expect(described_class.different_with_pubid.map { |d| d[:id] })
+        .to include("NISTIR 8379")
+    end
+  end
+
+  describe "#parse_fail_with_pubid" do
+    it "returns identifiers fail to parse" do
+      expect(described_class.parse_fail_with_pubid.map { |d| d[:id] })
+        .to include("NBS CIRC e2")
     end
   end
 end
