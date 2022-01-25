f11262d1a09c09ca5efeb2616aebac06c21169f8
Artur Komarov <artur.komarov@gmail.com>
2022-01-25 20:50:20 +0700

    NIST FIPS to NIST FIPS PUB series conversion to specific class

--- lib/nist_pubid/series/fips_pub.rb
+++ lib/nist_pubid/series/fips_pub.rb
@@ -1,10 +1,19 @@
 module NistPubid
   module Series
     class FipsPub < NistPubid::Serie
+      SERIE_REGEXP = /NIST FIPS( PUB)?/.freeze
       EDITION_REGEXP = /[0-9]+[A-Za-z]*(?<prepend>-)(?<sequence>\d+)/.freeze
 
       def parse_docnumber(code, code_original)
-        super(code.gsub("NIST FIPS PUB 54-Jan15", "NIST FIPS PUB 54"), code_original)
+        super(code.gsub("NIST FIPS 54-Jan15", "NIST FIPS 54"), code_original)
+      end
+
+      def self.match?(code)
+        SERIE_REGEXP.match?(code)
+      end
+
+      def self.parse(code)
+        new(serie: "FIPS PUB", parsed: SERIE_REGEXP.match(code).to_s)
       end
     end
   end
