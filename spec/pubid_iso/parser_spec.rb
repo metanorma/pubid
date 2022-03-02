4a0c6e6ffe592b2f489cd28c9e22a63aa2d508d7
Artur Komarov <artur.komarov@gmail.com>
2022-03-02 20:16:33 +0700

    Add test for sample identifiers parsing

--- spec/pubid_iso/parser_spec.rb
+++ spec/pubid_iso/parser_spec.rb
@@ -1,11 +1,23 @@
+require "parslet/rig/rspec"
+
 RSpec.describe PubidIso::Parser do
-  subject { described_class.new.parse(pubid) }
+  subject { described_class.new }
 
   context "ISO 4" do
     let(:pubid) { "ISO 4" }
 
     it "parses document id" do
-      expect(subject).to eq(number: "4")
+      expect(subject.parse(pubid)).to eq(number: "4")
+    end
+  end
+
+  context "iso-sample-identifiers-v2.txt" do
+    it "parses identifiers from iso-sample-identifiers-v2.txt" do
+      f = open("spec/fixtures/iso-sample-identifiers-v2.txt")
+      f.readlines.each do |pub_id|
+        #expect()
+        expect(subject).to parse(pub_id.chomp)
+      end
     end
   end
 end
