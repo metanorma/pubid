9969f137d9de2ac13a6fc54b8897251fdb13804c
Ronald Tse <ronald.tse@ribose.com>
2022-03-06 14:25:01 +0800

    Update identifiers, split off PubIDs with IWA and languages

--- spec/pubid_iso/parser_spec.rb
+++ spec/pubid_iso/parser_spec.rb
@@ -4,24 +4,40 @@ RSpec.describe PubidIso::Parser do
   subject { described_class.new }
 
   context "Identifiers list from file" do
-    it "parses identifiers from iso-sample-identifiers-v2.txt" do
-      f = open("spec/fixtures/iso-sample-identifiers-v2.txt")
+    it "parses identifiers from iso-pubid-basic.txt" do
+      f = open("spec/fixtures/iso-pubid-basic.txt")
       f.readlines.each do |pub_id|
         next if pub_id.match?("^#")
         expect(subject).to parse(pub_id.chomp)
       end
     end
 
-    it "parses identifiers from iso-sample-identifiers-additional.txt" do
-      f = open("spec/fixtures/iso-sample-identifiers-additional.txt")
+    it "parses identifiers from iso-pubid-nsb.txt" do
+      f = open("spec/fixtures/iso-pubid-nsb.txt")
       f.readlines.each do |pub_id|
         next if pub_id.match?("^#")
         expect(subject).to parse(pub_id.chomp)
       end
     end
 
-    it "parses identifiers from iso-sample-identifiers-coramd.txt" do
-      f = open("spec/fixtures/iso-sample-identifiers-coramd.txt")
+    it "parses identifiers from iso-pubid-coramd.txt" do
+      f = open("spec/fixtures/iso-pubid-coramd.txt")
+      f.readlines.each do |pub_id|
+        next if pub_id.match?("^#")
+        expect(subject).to parse(pub_id.chomp)
+      end
+    end
+
+    xit "parses identifiers from iso-pubid-languages.txt" do
+      f = open("spec/fixtures/iso-pubid-languages.txt")
+      f.readlines.each do |pub_id|
+        next if pub_id.match?("^#")
+        expect(subject).to parse(pub_id.chomp)
+      end
+    end
+
+    xit "parses identifiers from iwa-pubid.txt" do
+      f = open("spec/fixtures/iwa-pubid.txt")
       f.readlines.each do |pub_id|
         next if pub_id.match?("^#")
         expect(subject).to parse(pub_id.chomp)
