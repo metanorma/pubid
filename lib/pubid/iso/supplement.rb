81fe6df3e4926d60c60605dc7acf36fc296c4d14
Artur Komarov <artur.komarov@gmail.com>
2022-06-13 12:27:31 +0700

    Add comparator method for supplement's stage

--- lib/pubid/iso/supplement.rb
+++ lib/pubid/iso/supplement.rb
@@ -1,11 +1,22 @@
 module Pubid::Iso
   class Supplement < Pubid::Core::Supplement
+    attr_accessor :stage
+
+    def initialize(stage: nil, **args)
+      super(**args)
+      @stage = stage
+    end
+
     def render_pubid_stage
       ((@stage && @stage) || "")
     end
 
     def render_urn_stage
       ((@stage && ":stage-#{sprintf('%05.2f', Pubid::Core::Renderer::Urn::STAGES[@stage.to_sym])}") || "")
+    end
+
+    def <=>(other)
+      (super == 0) && ((stage.nil? || stage == other.stage) && 0 || -1) || super
     end
   end
 end
--- spec/pubid_iso/supplemnent_spec.rb
+++ spec/pubid_iso/supplemnent_spec.rb
@@ -1,39 +1,38 @@
 RSpec.describe Pubid::Iso::Supplement do
+  describe "rendering" do
+    subject do
+      Pubid::Iso::Supplement.new(version: 1, number: supplement_year, stage: supplement_stage)
+    end
+
+    context "when supplement has stage" do
+      let(:supplement_year) { 2000 }
+      let(:supplement_stage) { "CD" }
+
+      let(:pubid_number) { "1:2000" }
+      let(:urn_stage) { ":stage-30.00" }
+      let(:urn_number) { ":2000:v1" }
+
+      it { expect(subject.render_pubid_stage).to eq(supplement_stage) }
+
+      it { expect(subject.render_urn_stage).to eq(urn_stage) }
+    end
+  end
+
   describe "#==" do
     subject do
       first_supplement == second_supplement
     end
 
     let(:first_supplement) do
-      Pubid::Iso::Supplement.new(version: 1, number: first_supplement_year, stage: first_supplement_stage)
+      described_class.new(version: 1, number: first_supplement_year, stage: first_supplement_stage)
     end
     let(:second_supplement) do
-      Pubid::Iso::Supplement.new(version: 1, number: second_supplement_year, stage: second_supplement_stage)
+      described_class.new(version: 1, number: second_supplement_year, stage: second_supplement_stage)
     end
     let(:first_supplement_year) { nil }
     let(:second_supplement_year) { nil }
     let(:first_supplement_stage) { nil }
     let(:second_supplement_stage) { nil }
-
-    context "when have equal supplement version and year" do
-      let(:first_supplement_year) { 2000 }
-      let(:second_supplement_year) { 2000 }
-
-      it { is_expected.to be_truthy }
-    end
-
-    context "when query don't have a year" do
-      let(:second_supplement_year) { 2000 }
-
-      it { is_expected.to be_truthy }
-    end
-
-    context "when years is different" do
-      let(:first_supplement_year) { 2001 }
-      let(:second_supplement_year) { 2000 }
-
-      it { is_expected.to be_falsey }
-    end
 
     context "when query without stage but stage in results" do
       let(:second_supplement_stage) { "CD" }
@@ -54,26 +53,6 @@ RSpec.describe Pubid::Iso::Supplement do
         it { is_expected.to be_falsey }
       end
     end
-  end
 
-  describe "rendering" do
-    subject do
-      Pubid::Iso::Supplement.new(version: 1, number: supplement_year, stage: supplement_stage)
-    end
-
-    context "when supplement has stage" do
-      let(:supplement_year) { 2000 }
-      let(:supplement_stage) { "CD" }
-
-      let(:pubid_number) { "1:2000" }
-      let(:urn_stage) { ":stage-30.00" }
-      let(:urn_number) { ":2000:v1" }
-
-      it { expect(subject.render_pubid_stage).to eq(supplement_stage) }
-      it { expect(subject.render_pubid_number).to eq(pubid_number) }
-
-      it { expect(subject.render_urn_stage).to eq(urn_stage) }
-      it { expect(subject.render_urn_number).to eq(urn_number) }
-    end
   end
 end
