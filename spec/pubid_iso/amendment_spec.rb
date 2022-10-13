b63cb86e998ed5a2ced76c8ea8987960b39cb1ce
Artur Komarov <artur.komarov@gmail.com>
2022-10-13 14:39:30 +0700

    Update amendment and corrigendum tests

--- spec/pubid_iso/amendment_spec.rb
+++ spec/pubid_iso/amendment_spec.rb
@@ -1,18 +1,20 @@
-RSpec.describe Pubid::Iso::Amendment do
-  let(:pubid) { "ISO 6709:2008#{pubid_amendment}" }
-  let(:pubid_amendment) { "/Amd 1:2009" }
+module Pubid::Iso
+  RSpec.describe Amendment do
+    let(:pubid_amendment) { "/Amd 1:2009" }
 
-  describe "#render" do
-    subject { Pubid::Iso::Identifier.parse(pubid).amendments.first }
-
-    it { expect(subject.render_pubid).to eq(pubid_amendment) }
-
-    context "when amendment has stage" do
-      let(:pubid_amendment) { "/CD Amd 1:2009" }
-      let(:urn_amendment) { ":stage-30.00:amd:2009:v1" }
+    describe "#render" do
+      subject { described_class.new(number: 1, year: 2009) }
 
       it { expect(subject.render_pubid).to eq(pubid_amendment) }
-      it { expect(subject.render_urn).to eq(urn_amendment) }
+
+      context "when amendment has stage" do
+        subject { described_class.new(number: 1, year: 2009, stage: :CD) }
+        let(:pubid_amendment) { "/CD Amd 1:2009" }
+        let(:urn_amendment) { ":stage-30.00:amd:2009:v1" }
+
+        it { expect(subject.render_pubid).to eq(pubid_amendment) }
+        it { expect(subject.render_urn).to eq(urn_amendment) }
+      end
     end
   end
 end
--- spec/pubid_iso/corrigendum_spec.rb
+++ spec/pubid_iso/corrigendum_spec.rb
@@ -1,18 +1,20 @@
-RSpec.describe Pubid::Iso::Corrigendum do
-  let(:pubid) { "ISO 6709:2008#{pubid_corrigendum}" }
-  let(:pubid_corrigendum) { "/Cor 1:2009" }
+module Pubid::Iso
+  RSpec.describe Corrigendum do
+    let(:pubid_corrigendum) { "/Cor 1:2009" }
 
-  describe "#render" do
-    subject { Pubid::Iso::Identifier.parse(pubid).corrigendums.first }
-
-    it { expect(subject.render_pubid).to eq(pubid_corrigendum) }
-
-    context "when corrigendum has stage" do
-      let(:pubid_corrigendum) { "/CD Cor 1:2009" }
-      let(:urn_corrigendum) { ":stage-30.00:cor:2009:v1" }
+    describe "#render" do
+      subject { described_class.new(number: 1, year: 2009) }
 
       it { expect(subject.render_pubid).to eq(pubid_corrigendum) }
-      it { expect(subject.render_urn).to eq(urn_corrigendum) }
+
+      context "when corrigendum has stage" do
+        subject { described_class.new(number: 1, year: 2009, stage: :CD) }
+        let(:pubid_corrigendum) { "/CD Cor 1:2009" }
+        let(:urn_corrigendum) { ":stage-30.00:cor:2009:v1" }
+
+        it { expect(subject.render_pubid).to eq(pubid_corrigendum) }
+        it { expect(subject.render_urn).to eq(urn_corrigendum) }
+      end
     end
   end
 end
