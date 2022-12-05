02dbd67476d86e075bf9830376c3f3d72ec836a9
Artur Komarov <artur.komarov@gmail.com>
2022-12-05 13:46:19 +0700

    Update corrigendum's tests

--- spec/pubid_iso/identifier/corrigendum_spec.rb
+++ spec/pubid_iso/identifier/corrigendum_spec.rb
@@ -1,12 +1,13 @@
 module Pubid::Iso
   module Identifier
-    RSpec.describe Amendment do
+    RSpec.describe Corrigendum do
       context "when create corrigendum identifier" do
-        let(:base) { described_class.new(**{ number: number }.merge(params)) }
+        let(:base) { Identifier::Base.create(**{ number: number }.merge(params)) }
 
         subject { described_class.new(type: :cor, number: 1, base: base, **corrigendum_params) }
 
         let(:params) { { year: 1999 } }
+        let(:number) { 123 }
 
         context "when corrigendum with stage" do
           let(:corrigendum_params) { { stage: stage } }
@@ -14,7 +15,7 @@ module Pubid::Iso
           context "with DCor stage" do
             let(:stage) { :dcor }
             it "renders long stage and corrigendum" do
-              expect(subject.to_s).to eq("ISO #{number}:1999/DCor 1")
+              expect(subject.to_s).to eq("ISO #{number}:1999/DCOR 1")
             end
 
             it "renders short stage and corrigendum" do
