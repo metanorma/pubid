48df77c5b4f6f65b71eef7a4105bb9ca793d7847
Artur Komarov <artur.komarov@gmail.com>
2022-12-13 14:49:50 +0700

    Add missing typed stages: FDPAS, DGuide, FDGuide

--- lib/pubid/iso/identifier/guide.rb
+++ lib/pubid/iso/identifier/guide.rb
@@ -3,7 +3,18 @@ module Pubid::Iso
     class Guide < Base
       def_delegators 'Pubid::Iso::Identifier::Guide', :type
 
-      TYPED_STAGES = {}.freeze
+      TYPED_STAGES = {
+        dguide: {
+          abbr: "DGuide",
+          name: "Draft Guide",
+          harmonized_stages: %w[40.00 40.20 40.60 40.92 40.93 50.00 50.20 50.60 50.92],
+        },
+        fdguide: {
+          abbr: "FDGuide",
+          name: "Final Draft Guide",
+          harmonized_stages: %w[50.00 50.20 50.60 50.92],
+        },
+      }.freeze
 
       def self.get_renderer_class
         Renderer::Guide
--- lib/pubid/iso/identifier/publicly_available_specification.rb
+++ lib/pubid/iso/identifier/publicly_available_specification.rb
@@ -6,9 +6,13 @@ module Pubid::Iso
       TYPED_STAGES = {
         dpas: {
           abbr: "DPAS",
-          type: :pas,
           name: "Publicly Available Specification Draft",
           harmonized_stages: %w[40.00 40.20 40.60 40.92 40.93 50.00 50.20 50.60 50.92],
+        },
+        fdpas: {
+          abbr: "FDPAS",
+          name: "Publicly Available Specification Final Draft",
+          harmonized_stages: %w[50.00 50.20 50.60 50.92],
         },
       }.freeze
 
