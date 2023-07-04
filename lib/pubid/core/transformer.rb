881cff4bda9f79927cb9e3c2114422c11d71a2b1
Artur Komarov <artur.komarov@gmail.com>
2023-07-04 12:36:56 +0700

    Move roman to integer conversion from pubid-iso

--- lib/pubid/core/transformer.rb
+++ lib/pubid/core/transformer.rb
@@ -41,6 +41,29 @@ module Pubid::Core
         end
         context
       end
+
+      rule(roman_numerals: simple(:roman_numerals)) do |context|
+        roman_to_int(context[:roman_numerals])
+      end
+    end
+
+    ROMAN_TO_INT = {
+      "I" => 1,
+      "V" => 5,
+      "X" => 10,
+      "L" => 50,
+      "C" => 100,
+      "D" => 500,
+      "M" => 1000,
+    }
+
+    def roman_to_int(roman)
+      sum = ROMAN_TO_INT[roman.to_s[0]]
+      roman.to_s.chars.each_cons(2) do |c1, c2|
+        sum += ROMAN_TO_INT[c2]
+        sum -= ROMAN_TO_INT[c1] * 2 if ROMAN_TO_INT[c1] < ROMAN_TO_INT[c2]
+      end
+      sum
     end
   end
 end
