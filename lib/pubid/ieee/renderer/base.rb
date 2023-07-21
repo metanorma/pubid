3496e5e24a6e1394372b73ec86dcfee213ea616c
Artur Komarov <artur.komarov@gmail.com>
2023-07-22 08:33:42 +0900

    Convert month to integer to avoid error in ruby 3.2

--- lib/pubid/ieee/renderer/base.rb
+++ lib/pubid/ieee/renderer/base.rb
@@ -77,7 +77,7 @@ module Pubid::Ieee::Renderer
       if edition[:month]
         month = edition[:month]
         month = Date.parse(edition[:month]).month if month.to_i.zero?
-        result += "-#{sprintf('%02d', month)}"
+        result += "-#{sprintf('%02d', month.to_i)}"
       end
       result += "-#{edition[:day]}" if edition[:day]
       result
